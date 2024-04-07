98b17470ea50c4f8a43e6476742d0ec589b2e6d5
CrespoDB <frederikdb@outlook.dk>
2024-04-07 19:04:40 +0200

    Transparent NVIM, Alpha instead of Dashboard

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,12 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		-- Setup catppuccin with transparent background support
+		require("catppuccin").setup({
+			transparent_background = true,
+			-- Add any additional configuration options here
+		})
+		-- Applying the colorscheme
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
