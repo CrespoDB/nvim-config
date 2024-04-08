dcf071edd3dddc3150dbaa6f4f1c5df317169931
CrespoDB <frederikdb@outlook.dk>
2024-04-08 23:49:47 +0200

    Added new UI

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,77 +1,133 @@
+--- @type LazyPluginSpec
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter", -- load plugin after all configuration is set
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    require("alpha.term")
+    local arttoggle = false
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+    local logo = {
+      [[                                                    ]],
+      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+      [[                                                    ]],
+    }
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
+    local art = {
+      -- { name, width, height }
+      { "tohru", 62, 17 },
+    }
 
-		_Gopts = {
-			position = "center",
-			hl = "Type",
-			-- wrap = "overflow";
-		}
+    if arttoggle == true then
+      dashboard.opts.opts.noautocmd = true
+      dashboard.section.terminal.opts.redraw = true
+      local path = vim.fn.stdpath("config") .. "/assets/"
+      -- local random = math.random(1, #art)
+      local currentart = art[1]
+      dashboard.section.terminal.command = "cat " .. path .. currentart[1]
 
-		-- Set menu
-		dashboard.section.buttons.val = {
-			-- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
-			dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
-			dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
-			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-			dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
-			dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
-			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
-			dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
-			dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
-		}
+      dashboard.section.terminal.width = currentart[2]
+      dashboard.section.terminal.height = currentart[3]
 
-		local function footer()
-			return "Federico Dalrosini"
-		end
-		dashboard.section.footer.val = footer()
+      dashboard.opts.layout = {
+        dashboard.section.terminal,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        dashboard.section.footer,
+      }
+    else
+      dashboard.section.header.val = logo
+    end
+    dashboard.section.buttons.val = {
+      -- dashboard.button("SPC j", "󰈚 Restore Session", ":SessionRestore<cr>"),
+      dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", " Find file", ":Telescope find_files<CR>"),
+      dashboard.button("g", "󰱼 Find word", ":Telescope live_grep<CR>"),
+      dashboard.button("r", " Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("c", " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("m", "󱌣 Mason", ":Mason<CR>"),
+      dashboard.button("l", "󰒲 Lazy", ":Lazy<CR>"),
+      dashboard.button("u", "󰂖 Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+      dashboard.button("q", " Quit NVIM", ":qa<CR>"),
+    }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "Function"
+    dashboard.section.buttons.opts.hl = "Identifier"
+    dashboard.section.footer.opts.hl = "Function"
+    dashboard.opts.layout[1].val = 4
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    if vim.o.filetype == "lazy" then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AlphaReady",
+        callback = function()
+          require("lazy").show()
+        end,
+      })
+    end
 
-		dashboard.opts.opts.noautocmd = true
-		alpha.setup(dashboard.opts)
+    local function getGreetingMessage()
+      local hour = tonumber(os.date("%H"))
+      local greeting = "Good day" -- Default greeting
 
-		require("alpha").setup(dashboard.opts)
+      -- Determine part of the day
+      if hour < 12 then
+        greeting = "Good morning ☕"
+      elseif hour >= 12 and hour < 18 then
+        greeting = "Good afternoon 🧋"
+      else
+        greeting = "Good evening 🍵"
+      end
+      return greeting
+    end
 
-		-- vim.api.nvim_create_autocmd("User", {
-		-- 	pattern = "LazyVimStarted",
-		-- 	callback = function()
-		-- 		local stats = require("lazy").stats()
-		-- 		local count = (math.floor(stats.startuptime * 100) / 100)
-		-- 		dashboard.section.footer.val = {
-		-- 			"󱐌 " .. stats.count .. " plugins loaded in " .. count .. " ms",
-		-- 			" ",
-		-- 			"      Mohammed Babiker Babai",
-		-- 		}
-		-- 		pcall(vim.cmd.AlphaRedraw)
-		-- 	end,
-		-- })
-	end,
+    require("alpha").setup(dashboard.opts)
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimStarted",
+      callback = function()
+        local v = vim.version()
+        local dev = ""
+        if v.prerelease == "dev" then
+          dev = "-dev+" .. v.build
+        else
+          dev = ""
+        end
+
+        local version = v.major .. "." .. v.minor .. "." .. v.patch .. dev
+        local stats = require("lazy").stats()
+        local plugins_count = stats.loaded .. "/" .. stats.count
+        local ms = math.floor(stats.startuptime + 0.5)
+        local time = vim.fn.strftime("%H:%M:%S")
+        local date = vim.fn.strftime("%d.%m.%Y")
+        local line1 = " " .. plugins_count .. " plugins loaded in " .. ms .. "ms"
+        local line2 = "󰃭 " .. date .. "  " .. time
+        local line3 = " " .. version
+        local line4 = getGreetingMessage()
+
+        local line1_width = vim.fn.strdisplaywidth(line1)
+        local line2Padded = string.rep(" ", (line1_width - vim.fn.strdisplaywidth(line2)) / 2) .. line2
+        local line3Padded = string.rep(" ", (line1_width - vim.fn.strdisplaywidth(line3)) / 2) .. line3
+        local line4Padded = string.rep(" ", (line1_width - vim.fn.strdisplaywidth(line4)) / 2) .. line4
+
+        dashboard.section.footer.val = {
+          line1,
+          line2Padded,
+          line3Padded,
+          line4Padded,
+        }
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
+  end,
 }
