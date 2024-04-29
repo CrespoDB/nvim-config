df04aea67c52168b7250706b66bf070e21895a4f
CrespoDB <frederikdb@outlook.dk>
2024-04-29 20:26:41 +0200

    Added rust and clang

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,27 +1,31 @@
 return {
-	"nvimtools/none-ls.nvim", -- Ensure this points to the correct null-ls repository
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",          -- Correct repository for none-ls
+  config = function()
+    local null_ls = require("null-ls") -- using the existing variable name as per your setup
 
-		null_ls.setup({
-			sources = {
-				-- Formatting sources
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.gofmt,
-				-- Diagnostics source
-				null_ls.builtins.diagnostics.golangci_lint.with({
-					extra_args = { "--fast" },
-				}),
-			},
-		})
+    null_ls.setup({
+      sources = {
+        -- Existing formatting sources
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.gofmt,
+        -- Existing diagnostics source
+        null_ls.builtins.diagnostics.golangci_lint.with({
+          extra_args = { "--fast" },
+        }),
+        -- Adding C/C++ formatting
+        null_ls.builtins.formatting.clang_format.with({
+          extra_args = { "--style=llvm" }, -- Customize the style as needed
+        }),
+      },
+    })
 
-		-- Auto-format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = { "*.go", "*.lua" },
-			callback = function()
-				vim.lsp.buf.format()
-			end,
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    -- Auto-format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.go", "*.lua", "*.c", "*.cpp" }, -- Include C and C++ files
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
